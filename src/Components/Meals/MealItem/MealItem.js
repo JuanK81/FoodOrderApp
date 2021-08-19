2d4e7d8e53b690106d29fb7b44e02ee0188b6542
Juan Casals <jcasalsbadolato@gmail.com>
2021-08-19 20:27:22 +0200

    useContext setup

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -2,7 +2,7 @@ import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
-    const price = `$${props.price.toFixed(2)}`;
+    const price = `${props.price.toFixed(2)}€`;
     return <li className={classes.meal}>
         <div>
             <h3>{props.name}</h3>
