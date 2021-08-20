d0b3393dbc08b8361ad1491cec614eee52467aad
Juan Casals <jcasalsbadolato@gmail.com>
2021-08-20 20:19:49 +0200

    Cart logic done

--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,33 @@
-
+import React, { useRef, useState } from 'react';
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
 
 const MealItemForm = (props) => {
-    return <form className={classes.form}>
+    const [amountIsValid, setAmountIsValid] = useState(true)
+    const amountInputRef = useRef();
+
+
+    const submitHnadler = (event) => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if (
+        enteredAmount.trim().length === 0 || 
+        enteredAmountNumber < 1 || 
+        enteredAmountNumber > 5
+        ) {
+            setAmountIsValid = false;
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+    };
+
+    return <form className={classes.form} onSubmit={submitHnadler}>
         <Input 
+        ref={amountInputRef}
         label="Amount"
         input={{
             id: 'amount_' + props.id,
@@ -16,6 +39,7 @@ const MealItemForm = (props) => {
         }}
         />
         <button>+ Add</button>
+        {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
 };
 
