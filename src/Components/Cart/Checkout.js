1de05360e96f53e07e661df6dd9250c29b24044d
Juan <jcasalsbadolato@gmail.com>
2021-11-16 12:57:13 +0100

    Fix prblems with CSS, adapt layout to samller screens, add brackepoints at 800 and 650 px heigth

--- src/Components/Cart/Checkout.js
+++ src/Components/Cart/Checkout.js
@@ -5,29 +5,35 @@ const Checkout = (props) => {
         event.preventDefault();
 
         
+        
     };
     return (
-        <form>
-            <div className={classes.control}>
-                <label htmlFor="name">Your Name</label>
-                <input type="text" id="name" />
-            </div>
-            <div className={classes.control}>
-                <label htmlFor="street">Street</label>
-                <input type="text" id="street" />
-            </div>
-            <div className={classes.control}>
-                <label htmlFor="postal">Postal Code</label>
-                <input type="text" id="postal" />
-            </div>
-            <div className={classes.control}>
-                <label htmlFor="city">City</label>
-                <input type="text" id="city" />
-            </div>
-            <button type="button" onClick={props.onCancel}>Cancel</button>
-            <button onClick={confirmHandler}>Confirm</button>
-        </form>
-    )
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={classes.control}>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='street'>Street</label>
+        <input type='text' id='street' />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='postal'>Postal Code</label>
+        <input type='text' id='postal' />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='city'>City</label>
+        <input type='text' id='city' />
+      </div>
+      <div className={classes.actions}>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
 };
+
 
 export default Checkout;
