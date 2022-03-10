119aa0026c0b4e3ac8c90561e6042086ff5c775a
JuanK81 <jcasalsbadolato@gmail.com>
2022-03-10 12:16:11 +0100

    update firebase

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -15,7 +15,10 @@ const AvailableMeals = () => {
       const fetchMeals = async () => {
         setIsLoading(true);
 
-        const response = await fetch('https://reactmeals-49c7c-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+        // const response = await fetch('https://reactmeals-49c7c-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+        const response = await fetch(
+          'https://meals-2-be318-default-rtdb.firebaseio.com/meals.json'
+        );
 
         if (!response.ok) {
           throw new Error('Something went wrong!')
