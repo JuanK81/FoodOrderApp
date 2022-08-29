b9ed30f5b532d8666a7275ba8b03a08cc80478d7
JuanK81 <82944967+JuanK81@users.noreply.github.com>
2022-08-29 10:32:41 +0200

    Update AvailableMeals.js

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -17,7 +17,7 @@ const AvailableMeals = () => {
 
         // const response = await fetch('https://reactmeals-49c7c-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
         const response = await fetch(
-          'https://meals-update-default-rtdb.firebaseio.com/meals.json'
+          'https://meals-update-default-rtdb.firebaseio.com/meals/meals.json'
         );
 
         if (!response.ok) {
